e93c35e782908287430ba61955fc2fc1e2018b4d
Jorge Garcia <jigarcian@users.noreply.github.com>
2014-11-09 14:52:10 -0430

    Update plot4.R

--- scripts/plot4.R
+++ scripts/plot4.R
@@ -1,13 +1,10 @@
-# set working directory (change this to fit your needs)
-setwd('~/Source Code/GitHub/Exploratory-Data-Analysis')
-
 # make sure the plots folder exists
 if (!file.exists('plots')) {
   dir.create('plots')
 }
 
 # load data
-source('scripts/get_and_clean_data.R')
+source('/Coursera/Power.R')
 
 # open device
 png(filename='plots/plot4.png',width=480,height=480,units='px')
@@ -33,3 +30,7 @@ plot(power.consumption$DateTime,power.consumption$GlobalReactivePower,xlab='date
 
 # close device
 x<-dev.off()
+
+
+
+
